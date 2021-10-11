2c5bc3150808d255327e39a1b8f9ab92e790b788
Lam <76472734+ngld0@users.noreply.github.com>
2021-10-11 17:53:27 +0800

    Create 965. isUnivalTree.cpp

---
+++ 965. isUnivalTree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        
+        return check(root, root->val);
+    }
+    bool check(TreeNode* root, int val){
+        
+        if(!root) return true;
+        if(root->val != val) return false;
+        return check(root->left, val) && check(root->right, val);
+    }
+};
+/*
+A binary tree is uni-valued if every node in the tree has the same value.
+
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,1,1,1,1,null,1]
+Output: true
+Example 2:
+
+
+Input: root = [2,2,2,5,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+0 <= Node.val < 100
+*/
