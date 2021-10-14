edb9b2360ef6f2e1c453ac4e917896572cac896d
Lam <76472734+ngld0@users.noreply.github.com>
2021-10-14 13:19:07 +0800

    Create 279. numSquares.cpp

---
+++ 279. numSquares.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        while (n % 4 == 0) n /= 4;
+        if (n % 8 == 7) return 4;
+        for (int a = 0; a * a <= n; ++a) {
+            int b = sqrt(n - a * a);
+            if (a * a + b * b == n) {
+                return !!a + !!b;
+            }
+        }
+        return 3; 
+    }
+    //dynamic programming
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; i + j * j <= n; ++j) {
+                dp[i + j * j] = min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp.back();
+    }
+};
+
+/*
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+ 
+
+Constraints:
+
+1 <= n <= 104
+
+*/
