0d585fb2a3b3779fef09f40d5c7c52e77ff1c305
Lam <76472734+ngld0@users.noreply.github.com>
2021-10-23 01:04:29 +0800

    Create 38. countAndSay.cpp

---
+++ 38. countAndSay.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    
+    string countAndSay(int n) {
+        string s="1";
+        for(int ii=1;ii<n;ii++)
+        {
+            int n=s.size();
+            int i=0;
+            int j=0;
+            string ans="";
+            while(i<n)
+            {
+                int count=0;
+                char ch=s[i];
+                while(i<n&&ch==s[i])
+                {
+                    count++;
+                    i++;
+                }
+                ans+=to_string(count);
+                ans+=ch;
+            }
+            s=ans;
+        }
+        return s;
+    }
+};
+/*
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+Constraints:
+
+1 <= n <= 30
+*/
