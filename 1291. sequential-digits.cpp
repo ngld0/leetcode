3948a08a841121a517744ee4ab6346cb73f325e3
Lam <76472734+ngld0@users.noreply.github.com>
2022-01-23 14:37:52 +0800

    Create 1291. sequential-digits.cpp

---
+++ 1291. sequential-digits.cpp
@@ -0,0 +1,67 @@
+class Solution {
+   
+public:
+    //sol 1.
+    vector<int> init = {
+        12, 23, 34, 45, 56, 67, 78, 89,
+        123, 234, 345, 456, 567, 678, 789,
+        1234, 2345, 3456, 4567, 5678, 6789,
+        12345, 23456, 34567, 45678, 56789,
+        123456, 234567, 345678, 456789,
+        1234567, 2345678, 3456789,
+        12345678, 23456789,
+        123456789
+    };
+    
+	vector<int> sequentialDigits(int low, int high) {
+		vector<int> ans;
+        for (int n : init) { 
+            if (n >= low && n <= high)
+                ans.push_back(n);
+        }
+        return ans;
+	}
+//     // Sol 2.
+//     vector<int> sequentialDigits(int low, int high) {
+//         vector<int> ans;
+        
+//         for(int i=1;i<=8;i++){
+            
+//             int num=i;
+            
+//             for(int j=i+1;j<=9;j++){
+                
+//                 if(((num%10)+1)==j)
+//                     num=num*10+j;
+                
+//                 if(num>=low && num<=high) ans.push_back(num);
+//             }
+            
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+
+};
+
+/*
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ 
+
+Constraints:
+
+10 <= low <= high <= 10^9
+*/
