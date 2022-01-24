52faa1de0f24aa80908639d2a51f388347e85afe
Lam <76472734+ngld0@users.noreply.github.com>
2022-01-24 11:36:18 +0800

    Create 520. detect-capital.cpp

---
+++ 520. detect-capital.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // Sol 1.
+    bool detectCapitalUse(string word) {
+        
+        int flag = 0, FLAG = 0;
+        
+        for(int i = 1; i < word.size(); i++)
+        {
+            if(word[i] >= 'A' && word[i] <= 'Z')
+                FLAG = 1;
+            if(word[i] >= 'a' && word[i] <= 'z')
+                flag = 1;
+        }
+        
+        if(flag && FLAG)
+            return 0;
+        if(FLAG && word[0] >= 'a' && word[0] <= 'z')
+            return 0;
+        return 1;
+    }
+    //Sol 2.
+    bool detectCapitalUse(string word) {
+        for(int i = 1; i < word.size(); i++){
+            if(isupper(word[1]) != isupper(word[i]) || islower(word[0]) && isupper(word[i])) 
+                return false;
+        }
+        return true;
+
+    }
+};
