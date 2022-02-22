86a1083c3b224568570d98b906e15b97f4d3165d
Lam <76472734+ngld0@users.noreply.github.com>
2022-02-22 11:42:08 +0800

    Update 2180. count-integers-with-even-digit-sum.cpp

--- 2180. count-integers-with-even-digit-sum.cpp
+++ 2180. count-integers-with-even-digit-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int countEven(int num) {
         int cnt = 0;
         for(int i = 2; i <= num;i++){
@@ -16,5 +17,14 @@ public:
             num /= 10;
         }
         return sum %2 ==0 ? true:false;
+    }*/
+    // pattern solution. first, check whether sum of num degits are even?
+    int countEven(int num) {
+        int n = num, sum = 0;
+        while(n){
+            sum += n %10;
+            n /= 10;
+        }
+        return (num - sum%2)/2;
     }
 };
