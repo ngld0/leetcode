36aeefaaa40929a780dfe05002bc8eb25974de37
Lam <76472734+ngld0@users.noreply.github.com>
2022-03-28 14:48:19 +0800

    Update 1337. the-k-weakest-rows-in-a-matrix.cpp

--- 1337. the-k-weakest-rows-in-a-matrix.cpp
+++ 1337. the-k-weakest-rows-in-a-matrix.cpp
@@ -9,10 +9,17 @@ public:
         // sort in the first element of each vector!!! should to know about sort function of c++
         sort(mat.begin(),mat.end());
             
-        for(int i = 0;i< k ;i++){
-            ret.push_back(mat[i][mat.size()]);
-            
+        for(int i = 0;i < k ;i++){
+            ret.push_back(mat[i][mat[0].size()-1]);
         }
+        
+//         for(int i = 0;i< mat.size();i++){
+//             for(int j = 0 ; j < mat[0].size();j++){
+                
+//                 cout<< mat[i][j];
+//             }
+//             cout<< endl;
+//         }
         return ret;
     }
 
