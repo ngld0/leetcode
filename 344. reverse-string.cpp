8da8293f2838e13fe3490c7e2be78b45f71a4810
Lam <76472734+ngld0@users.noreply.github.com>
2022-04-01 11:51:43 +0800

    Create 344. reverse-string.cpp

---
+++ 344. reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size()-1;
+        while(left < right){
+            // char c = s[left];
+            // s[left] = s[right];
+            // s[right] = c;
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
