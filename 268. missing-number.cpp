f8b1c3c60861607f74599a6b49e5e549d3d2b662
Lam <76472734+ngld0@users.noreply.github.com>
2022-05-28 20:21:24 +0800

    Update 268. missing-number.cpp

--- 268. missing-number.cpp
+++ 268. missing-number.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        //math: 1 + 2+ ... + n = n(n+1)/2
+        int n = nums.size();
+        int sum = n * (n+1)/2;
+        for(int i =0;i<n;i++){
+            sum -= nums[i]; 
+        }
+        return sum;
+        
+        //bit manipulation
+        /*
         int mask =0, ans = nums.size();
         for (int i = 0; i < nums.size();i++){
             ans ^= nums[i];
             mask ^= i;
         }
         return mask^ans;
+        */
     }
 };
