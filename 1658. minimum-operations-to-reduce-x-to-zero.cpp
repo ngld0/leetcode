eb3ccf6b622b98941ab00f81731da307eb035f92
Lam <76472734+ngld0@users.noreply.github.com>
2022-06-12 16:04:24 +0800

    Update 1658. minimum-operations-to-reduce-x-to-zero.cpp

--- 1658. minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658. minimum-operations-to-reduce-x-to-zero.cpp
@@ -17,4 +17,34 @@ public:
                 sum += nums[l++];
         return res == INT_MAX ? -1 : res;
     }
+    /*
+    // sol 2. 
+    int minOperations(vector<int>& nums, int x) {
+        //slicing window
+       int totalsum = 0;
+        for(int i=0;i<nums.size();i++) 
+            totalsum += nums[i];
+        int sum = totalsum - x;
+        
+        if(sum < 0) 
+            return -1;
+        if(sum == 0) 
+            return nums.size();
+        int start = 0, add  = 0, len = -1;
+        // max size of window => min of the operation!!
+        for(int end = 0; end < nums.size(); end++)
+            {
+            if (add < sum)
+                add += nums[end];
+            while (add >= sum)
+            {
+                if (add == sum)
+                    len = max(len, end - start + 1);
+                add -= nums[start++];
+            }
+        }
+
+        return len == -1 ? -1 : nums.size() - len;
+    }
+    */
 };
