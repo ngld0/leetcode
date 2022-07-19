27a12f84983459a8419894bba22a97eefbe2ae04
Lam <76472734+ngld0@users.noreply.github.com>
2022-07-19 12:17:50 +0800

    Update 118. pascals-triangle.cpp

--- 118. pascals-triangle.cpp
+++ 118. pascals-triangle.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> tr(numRows);
-        for(int i=0;i<numRows;i++)
-        {
-            tr[i].resize(i+1);
-            tr[i][0]=tr[i][i]=1;
-            
-            for(int j=1;j<i;j++)
-            {
-              tr[i][j]=tr[i-1][j-1]+tr[i-1][j];
-            }
+        vector<vector<int>> r(numRows); //caused of space O(n)
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1); // set the size for each row
+            r[i][0] = 1; // set the first cell
+            r[i][i] = 1; // set the last cell
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j]; // calculate the current cell,
         }
-        return tr;
+        
+        return r;
     }
 };
-/*
-Given an integer numRows, return the first numRows of Pascal's triangle.
-
-In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
-
-
- 
-
-Example 1:
-
-Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-Example 2:
-
-Input: numRows = 1
-Output: [[1]]
- 
-
-Constraints:
-
-1 <= numRows <= 30
-*/
