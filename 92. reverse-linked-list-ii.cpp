32c9b07cc1bef47304f4bd14bfd6fe62b6b91db2
Lam <76472734+ngld0@users.noreply.github.com>
2022-07-21 14:06:50 +0800

    Update 92. reverse-linked-list-ii.cpp

--- 92. reverse-linked-list-ii.cpp
+++ 92. reverse-linked-list-ii.cpp
@@ -18,29 +18,44 @@ public:
         cout<<endl;
     }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+         // reversing the nodes in range left-right
+        /* Ex: 1-> 2 -> 3 -> 4->5 (left =2,right = 4)
+        step1. initialize
+        move the pointer to the left node and set it be current node,
+        the previous of current are pre node;
+        and the next of current node are forward node;
+        step2. reversing nodes in range [left,right]
+        try to reverse from the left to right node, you can follow the following problem first: https://leetcode.com/problems/reverse-linked-list/
+        ~> the following for loop are try to reverse the nodes in range of [left, right]
+        1-> 2 <- 3 <- 4->5 (2->5)
+        step3. 
+        set the head pointer to next and next
+        1->4->3->2->5
+        
+        */
+        //step 1;
         ListNode dummy(0);
         dummy.next = head;
 
         ListNode* h = &dummy;
-        
         for (int i = 1; i <left;i++) 
         {
             h = h->next;
         }
-
         ListNode* pre = h->next;
         
         ListNode* cur = pre->next;
-        // reversing the nodes in range left-right
-        for (int i =0 ; i <right -left;i++){
+ 
+        //step2
+        for (int i =0 ; i <right - left;i++){
             ListNode* nex = cur->next; //temporary node to save the node after swap; the saving node are after r node
             //cout << q->val << r->val <<t->val <<endl;
             cur->next = pre; // reversing the direction
             pre = cur; // 
             cur = nex; // set back the node after swap
         }
-        // after reversing, set the reversed sub list nodes to the parent listnode
-        h->next->next = cur; // set the last node after reverse
+        // step 3
+        h->next->next = cur; // set the next node after reverse
         h->next = pre;
         return dummy.next;
     }
